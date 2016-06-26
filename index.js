a08aa10fcea8dcee5dbeb60a2c36bb66669eac10
tpaivaa <tpaivaa@gmail.com>
2016-06-27 01:30:04 +0300

    siivousta

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+var storage = require('node-persist');
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
+storage.initSync();
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
@@ -11,19 +13,11 @@ server.listen(8080, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
+wsServer = new WebSocketServer({ httpServer: server });
 
 function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed
-    if (origin === "http:\/\/mc.ainaip.net") {
+  // put logic here to detect whether the specified origin is allowed.
+   if (origin === "http:\/\/mc.ainaip.net") {
          console.log("Correct Origin");
          return true;
     }
@@ -33,8 +27,10 @@ function originIsAllowed(origin) {
    }
 }
 
+var connections = {};
+var connectionIDCounter = 0;
+
 wsServer.on('request', function(request) {
-    console.log(1, request);
     if (!originIsAllowed(request.origin)) {
       // Make sure we only accept requests from an allowed origin
       request.reject();
@@ -42,19 +38,39 @@ wsServer.on('request', function(request) {
       return;
     }
 
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
+    var connection = request.accept(null, request.origin);
+
+    // Store a reference to the connection using an incrementing ID
+    connection.id = connectionIDCounter ++;
+    connections[connection.id] = connection;
+
+    // Now you can access the connection with connections[id] and find out
+    // the id for a connection with connection.id
+
+    console.log((new Date()) + ' Connection ID ' + connection.id + ' accepted.');
     connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected. ' +
+                    "Connection ID: " + connection.id);
+
+        // Make sure to remove closed connections from the global pool
+        delete connections[connection.id];
     });
 });
+
+// Broadcast to all open connections
+function broadcast(data) {
+    Object.keys(connections).forEach(function(key) {
+        var connection = connections[key];
+        if (connection.connected) {
+            connection.send(data);
+        }
+    });
+}
+
+// Send a message to a connection by its connectionID
+function sendToConnectionId(connectionID, data) {
+    var connection = connections[connectionID];
+    if (connection && connection.connected) {
+        connection.send(data);
+    }
+}
